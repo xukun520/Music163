56139168017d8d07e2b476d01c18b7bcfbc0a5b1
xu <xu@xus-MacBook-Air.local>
2021-11-04 19:51:23 +0800

    header,footer样式结构完成

--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,13 +1,57 @@
 import React, { memo } from 'react'
 import {NavLink} from 'react-router-dom'
-
+import { Input } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+import { HeaderLeft, HeaderRight, HeaderWrapper } from './style'
+//头部link数据
+import { headerLinks } from '@/common/local-data'
 export default memo(function WYYheader() {
-        return (
-            <div>
-               <NavLink to="/">主页</NavLink>
-               <NavLink to="/friends">朋友</NavLink>
-               <NavLink to="/find">发现</NavLink>
-            </div>
-        )
+      // Header-Select-Item
+  const showSelectItem = (item, index) => {
+    if (index < 3) {
+      return (
+        <NavLink
+          key={item.title}
+          to={item.link}
+          className="header-item"
+          exact
+          activeClassName="link-active"
+        >
+          <em>{item.title}</em>
+          <i className="icon"></i>
+        </NavLink>
+      )
+    } else {
+      return (
+        <a href={item.link} key={item.title} className="header-item">
+          {item.title}
+        </a>
+      )
     }
-)
+  }
+       // 返回的JSX
+  return (
+    <HeaderWrapper>
+      <div className="content w1100">
+        <HeaderLeft>
+          <h1>
+            <a href="#/" className="logo sprite_01">
+              网易云音乐
+            </a>
+          </h1>
+          <div className="header-group">
+            {headerLinks.map((item, index) => {
+              return showSelectItem(item, index)
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />} />
+          <div className="center">创作者中心</div>
+          <div>登录</div>
+        </HeaderRight>
+      </div>
+      <div className="red-line"></div>
+    </HeaderWrapper>
+  )
+})
