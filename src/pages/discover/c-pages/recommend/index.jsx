329dafd23eac0af823fb8a9c79bebd8f8729ff1e
xu <xu@xus-MacBook-Air.local>
2021-11-05 10:27:28 +0800

    hooks useDispatch，useSelector简化操作

--- src/pages/discover/c-pages/recommend/index.jsx
+++ src/pages/discover/c-pages/recommend/index.jsx
@@ -1,16 +1,24 @@
 import React,{memo, useEffect} from 'react'
 
-import {connect} from 'react-redux'
+import {connect,useDispatch,useSelector} from 'react-redux'
 
 import { getTopBannerAction } from './store/actionCreators'
 
 function WYYRecommend(props) {
+    //Allows you to extract data from the Redux store state
+    const {topBanners}=useSelector(state=>({
+        topBanners:state.recommend.topBanners
+    }))
+    //组件和redux关联 获取数据进行操作;
+     const dispatch =useDispatch()
+     //You may use it to dispatch actions as needed.
 
-    const {getBanners,topBanners}=props
-
+    // const {getBanners,topBanners}=props
+     //发送网络请求
+     //改变的时候不会影响组件重新渲染
     useEffect(()=>{
-        getBanners();
-    },[getBanners])
+       dispatch(getTopBannerAction())
+    },[dispatch])
 
     return (
         <div>
@@ -19,13 +27,15 @@ function WYYRecommend(props) {
     )
 }
 
-const mapStateToProps=state=>({
-    topBanners:state.recommend.topBanners
-})
-const mapDispatchTop=dispatch=>({
-    getBanners:()=>{
-        dispatch(getTopBannerAction())
-    }
-})
+// const mapStateToProps=state=>({
+//     topBanners:state.recommend.topBanners
+// })
+// const mapDispatchTop=dispatch=>({
+//     getBanners:()=>{
+//         dispatch(getTopBannerAction())
+//     }
+// })
 
-export default connect(mapStateToProps,mapDispatchTop)(memo(WYYRecommend))
+// export default connect(mapStateToProps,mapDispatchTop)(memo(WYYRecommend))
+
+export default memo(WYYRecommend)
